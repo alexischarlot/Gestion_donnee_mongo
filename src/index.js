13d556b5754fc41ef32e4d70a35f57acd0aa07bb
Jordan <jordanjezequel@bbox.fr>
2018-11-30 11:39:01 +0100

    Creation des velos

--- src/index.js
+++ src/index.js
@@ -4,32 +4,85 @@ let mustacheExpress = require('mustache-express');
 
 let app = express();
 let port = 80;
-let uri = 'mongodb://mongo:27017/test'; 
- 
+let uri = 'mongodb://mongo:27017/test';
+
 mongoose.connect(uri);
 
-app.engine('mustache', mustacheExpress()); 
+app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
 let path = require('path');
 app.use(express.static(path.join(__dirname + '/public')));
+const request = require('request');
 
-require('./models/dbModel')(mongoose);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
 
-app.get('/', function (req, res) {
-	console.log(req.query); //to get query in url
-	res.render('index', {start: req.query.start, end: req.query.end});
+	let positionSchema = new mongoose.Schema({
+		x: Number,
+		y: Number
+	})
+
+	let veloSchema = new mongoose.Schema({
+		name: String,
+		address: String,
+		position: positionSchema,
+		stands: Number,
+		bikes: Number
+	})
+
+	let Velo = mongoose.model('Velo', veloSchema);
+	let Position = mongoose.model('Position', positionSchema);
+
+
+	app.get('/', function (req, res) {
+		request('https://api.jcdecaux.com/vls/v1/stations?contract=Nancy&apiKey=76f63b7a53281f294547d0317b572d88c1aa041c', {
+			json: true
+		}, (err, res, body) => {
+			if (err) {
+				return console.log(err);
+			}
+			console.log(body);
+			body.forEach(element => {
+				let velo = new Velo({
+					name: element.name,
+					address: element.address,
+					position: {
+						x: element.position.lat,
+						y: element.position.lng
+					},
+					stands: element.available_bike_stands,
+					bikes: element.available_bikes
+				})
+				velo.save();
+			});
+		});
+		res.render('index', {
+			start: req.query.start,
+			end: req.query.end
+		});
+	});
 });
 
+
 app.get('/dep', function (req, res) {
-	q='/?start='+req.query.start+'&end='+req.query.end;
-	res.render('interest', {start: req.query.start, end: req.query.end, se: 1});
+	q = '/?start=' + req.query.start + '&end=' + req.query.end;
+	res.render('interest', {
+		start: req.query.start,
+		end: req.query.end,
+		se: 1
+	});
 });
 
 app.get('/ar', function (req, res) {
-	q='/?start='+req.query.start+'&end='+req.query.end;
-	res.render('interest', {start: req.query.start, end: req.query.end, se: 0});
+	q = '/?start=' + req.query.start + '&end=' + req.query.end;
+	res.render('interest', {
+		start: req.query.start,
+		end: req.query.end,
+		se: 0
+	});
 });
 
-app.listen(port);  
+app.listen(port);
--- src/models/dbModel.js
+++ src/models/dbModel.js
@@ -3,51 +3,20 @@ module.exports = function(mongoose){
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function() {
 
-        let prestationSchema = new mongoose.Schema({
-            title: String,
-            description: String,
-            image: String,
-            price: Number,
-            isVisible: Boolean
+        let positionSchema = new mongoose.Schema({
+            x: Number,
+            y: Number
         })
-    
-        let categorySchema = new mongoose.Schema({
-            title: String,
-            image: String,
-            prestations: [prestationSchema]
-        });
-    
-        let contributionSchema = new mongoose.Schema({
+
+        let veloSchema = new mongoose.Schema({
             name: String,
-            amount: Number,
-            message: String
+            address: String,
+            position: positionSchema,
+            stands: Number,
+            bikes: Number
         })
-    
-        let boxSchema = new mongoose.Schema({
-            recipientName: String,
-            recipientEmail: String,
-            message: String,
-            urlGift: String,
-            urlFund: String,
-            date: Date,
-            isPaid: Boolean,
-            isOpened: Boolean,
-            isCurrent: Boolean,
-            prestations: [prestationSchema],
-            contributions: [contributionSchema]
-        })	
-    
-        let userSchema = new mongoose.Schema({
-            password: String,
-            email: String,
-            boxes: [boxSchema],
-            isAdmin: Boolean
-        });
-    
-        let Box = mongoose.model('Box', boxSchema);
-        let User = mongoose.model('User', userSchema);
-        let Category = mongoose.model('Category', categorySchema);
-        let Prestation = mongoose.model('Prestation', prestationSchema);
-        let Contribution = mongoose.model('Contribution', contributionSchema);
+
+        let Velo = mongoose.model('Velo', veloSchema);
+        let Position = mongoose.model('Position', positionSchema);
     });
 }
