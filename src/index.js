dd26169f49e53694be3a51196250ab45a6ac5a23
Jordan <jordanjezequel@bbox.fr>
2018-11-30 11:53:47 +0100

    Parking

--- src/index.js
+++ src/index.js
@@ -33,18 +33,25 @@ db.once('open', function () {
 		bikes: Number
 	})
 
+	let parkingSchema = new mongoose.Schema({
+		name: String,
+		address: String,
+		position: positionSchema,
+		places: Number
+	})
+
 	let Velo = mongoose.model('Velo', veloSchema);
 	let Position = mongoose.model('Position', positionSchema);
+	let Parking = mongoose.model('Parking', parkingSchema);
 
 
 	app.get('/', function (req, res) {
-		request('https://api.jcdecaux.com/vls/v1/stations?contract=Nancy&apiKey=76f63b7a53281f294547d0317b572d88c1aa041c', {
+		/*request('https://api.jcdecaux.com/vls/v1/stations?contract=Nancy&apiKey=76f63b7a53281f294547d0317b572d88c1aa041c', {
 			json: true
 		}, (err, res, body) => {
 			if (err) {
 				return console.log(err);
 			}
-			console.log(body);
 			body.forEach(element => {
 				let velo = new Velo({
 					name: element.name,
@@ -57,6 +64,27 @@ db.once('open', function () {
 					bikes: element.available_bikes
 				})
 				velo.save();
+			});
+		});*/
+
+		request('https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=nom%2Cadresse%2Cplaces%2Ccapacite&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson', {
+			json: true
+		}, (err, res, body) => {
+			if (err) {
+				return console.log(err);
+			}
+			console.log(body.features);
+			body.features.forEach(element => {
+				let park = new Parking({
+					name: element.attributes.NOM,
+					address: element.attributes.ADRESSE,
+					position: {
+						x: element.geometry.x,
+						y: element.geometry.y
+					},
+					places: element.attributes.PLACES
+				})
+				park.save();
 			});
 		});
 		res.render('index', {
--- src/models/dbModel.js
+++ src/models/dbModel.js
@@ -16,7 +16,15 @@ module.exports = function(mongoose){
             bikes: Number
         })
 
+        let parkingSchema = new mongoose.Schema({
+            name: String,
+            address: String,
+            position: positionSchema,
+            places: Number
+        })
+
         let Velo = mongoose.model('Velo', veloSchema);
         let Position = mongoose.model('Position', positionSchema);
+        let Parking = mongoose.model('Parking', parkingSchema);
     });
 }
